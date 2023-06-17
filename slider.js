af646a272995e1c7aa47d6c19613a3e6496eebec
sasha_capsom <74817710+salyaami@users.noreply.github.com>
2023-06-18 01:10:49 +0300

    Add files via upload

--- slider.js
+++ slider.js
@@ -1,50 +1,16 @@
-// const images = document.querySelectorAll('.slider .slider-line img');
-// const sliderLine = document.querySelector('.slider-line');
-// let count = 0;
-// let width;
-//
-// function init() {
-//   console.log('resize');
-//   width = document.querySelector('.slider').offsetWidth;
-//   sliderLine.style.width = width*images.length + 'px';
-//   images.forEach(item => {
-//     item.style.width = width + 'px';
-//     item.style.height = 'auto';
-//   });
-//
-// }
-// window.addEventListener('resize', init);
-// init();
-//
-// document.querySelector('.swiper-button-next').addEventListener('click', function () {
-//   count ++;
-//   if (count >= images.length) {
-//     count = 0;
-//   }
-//   rollSlider();
-// });
-//
-// document.querySelector('.swiper-button-prev').addEventListener('click', function () {
-//   count--;
-//   if (count < 0 ) {
-//     count = images.length - 1;
-//   }
-//   rollSlider();
-// });
-//
-// function rollSlider() {
-//   sliderLine.style.transform = 'translate(-' + count * width + 'px)';
-// }
+var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: 1,
 
-new Swiper('.image-slider', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
-  },
-
-});
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+})
