e73fb4e2e69e17f45681e1f47428b283a4316f62
sasha_capsom <74817710+salyaami@users.noreply.github.com>
2023-06-18 00:02:54 +0300

    Add files via upload

--- slider.js
+++ slider.js
@@ -46,9 +46,5 @@ new Swiper('.image-slider', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev'
   },
-  // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
+
 });
